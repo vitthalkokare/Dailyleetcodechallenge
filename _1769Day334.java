593cc0309b39f662a2ef9f648977a8439d1773ff
Vitthal Kokare <87784273+vitthalkokare@users.noreply.github.com>
2025-01-10 02:13:28 +0530

    Day 334: Completed LeetCode challenge:1769. Minimum Number of Operations to Move All Balls to Each Box

--- _1769Day334.java
+++ _1769Day334.java
@@ -1,5 +1,5 @@
 // 1769. Minimum Number of Operations to Move All Balls to Each Box
-// https://leetcode.com/problems/minimum-number-of-operations-to-move-all-balls-to-each-box/description/
+// https://leetcode.com/problems/minimum-number-of-operations-to-move-all-balls-to-each-box
 
 import java.util.Arrays;
 
